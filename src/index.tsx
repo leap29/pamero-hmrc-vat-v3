b9fe8b9088e2af8bc92d125f348de38b3b66bf9b
Daniel King <leap29@gmail.com>
2021-08-13 19:57:33 +0100

    Fixed bootstrap css import

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import config from "./config";
 import registerServiceWorker from './registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
+
 ReactDOM.render(
   <Router>
     <App />
